79149b64813997fa2ef6f65f1182932b554d9d7b
Souheil CHELFOUH <trollfot@gmail.com>
2010-11-03 01:13:13 +0000

    Removed unused tests

--- src/grokcore/json/tests/security/not_a_permission_class_fixture.py
+++
@@ -1,8 +0,0 @@
-import grok
-
-class NotAPermissionSubclass(object):
-    grok.name('not really a permission')
-
-class MyRole(grok.Role):
-    grok.name('MyRole')
-    grok.permissions(NotAPermissionSubclass)
